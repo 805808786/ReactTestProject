b49f84c9485607dcf40776a17bcc37a46ac6205c
liutao <liutao@yicall.com>
2026-03-06 18:44:28 +0800

    更新企业变更API接口。

--- src/api/enterpriseChange.js
+++ src/api/enterpriseChange.js
@@ -2,10 +2,8 @@ import request from './request';
 
 /**
  * 根据日期获取企业变化信息
- * @param {string} changeDate - 日期字符串 'YYYY-MM-DD'
+ * @param {string} date - 日期字符串 'YYYY-MM-DD'
  */
-export function getByChangeDate(changeDate) {
-  return request.get('/backend/dataEnterpriseChange/getByChangeDate', {
-    params: { changeDate },
-  });
+export function getByChangeDate(date) {
+  return request.post('/backend/dataEnterpriseChange/getByChangeDate', { date });
 }
